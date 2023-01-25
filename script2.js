6c952b96ce2792a6b3e4cd0cbd5d7ede505028fd
joseignacio222 <123584544+joseignacio222@users.noreply.github.com>
2023-01-25 15:19:18 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+
+
+documento.getElementByid("boton").onclick=function () {
+
+console.log("Generar evento");
+
+document.getElementByid("demo").innerHTML="Dispongo de auto y licencia de conducir. Tengo posibilidad de cambiar de residencia. Tengo disponibilidad horaria. Obtuve premios y menciones en congresos a nivel internacional.";
+
+}
